4d4b9da253ae6e5b9ec34462484ad4a57157a4b4
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-01-06 12:52:50 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name="mangadexlib",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages("src"),
     install_requires=['cfscrape'],
     author="webmsgr",
     description="Small module for getting info about manga and chapters on Mangadex"
