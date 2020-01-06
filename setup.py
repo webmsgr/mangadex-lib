150a145c63d518b0fc93f424a5d7af02e14a191d
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-01-06 14:06:49 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name="mangadexlib",
     version="0.1",
-    python_requires="3",
+    python_requires=">3.6",
     packages=find_packages("src"),
     install_requires=['cfscrape'],
     package_dir={'':'src'},
