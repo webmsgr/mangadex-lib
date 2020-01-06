6a49a8c6645546d002d5047df15f6c352fcb7634
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-01-06 12:55:36 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     version="0.1",
     packages=find_packages("src"),
     install_requires=['cfscrape'],
+    package_dir={'':'src'},
     author="webmsgr",
     description="Small module for getting info about manga and chapters on Mangadex"
 )
