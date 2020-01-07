5d362e31d0e3af70d71f39602729daa5ec384c29
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-01-07 12:00:04 -0800

    Update main.py

--- src/mangasync/main.py
+++ src/mangasync/main.py
@@ -12,12 +12,14 @@ import mangadex
 class SyncedManga(mangadex.Manga):
     def __init__(self,id,remote=False):
         super().__init__(id,remote,False)
-    def changes(self,changed):
+    def changes(self,changed,langcode="gb"):
         deleted = []
         changed = []
         added = []
         for chapter in self.chapters:
             for lang in self.chapters[chapter]:
+                if lang != langcode:
+                    continue
                 for num,group in enumerate(self.chapters[chapter][lang]):
                     try:
                         if not changed.chapters[chapter][lang][num] == group:
